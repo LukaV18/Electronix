59b67a1b65d284268de7ac6e90aeea7c87721590
Luka Vitale <lukavitale9@gmail.com>
2022-06-16 14:35:56 -0300

    Solucion imagenes y prueba de ruta de archivos

--- Js/main.js
+++ Js/main.js
@@ -1,17 +1,17 @@
-let introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar");
+let introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
 
 while (isNaN(introduceTuEdad) <= 17) {
 
     if (isNaN(introduceTuEdad)) {
-        alert("Introduce solo números");
-        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar. Introduzca -ESC- para salir del ciclo");
+        alert("Por favor introduzca solo números.");
+        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
 
     } else if ((introduceTuEdad) < 18) {
 
-        alert("Tienes " + introduceTuEdad + " años,no tienes permitido ingresar a esta página")
-        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar. Introduzca -ESC- para salir del ciclo");
+        alert("Tienes " + introduceTuEdad + " años,no tienes permitido ingresar a esta página.")
+        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
     } else {
-        alert("Tienes " + introduceTuEdad + " años, puedes entrar a esta página");
+        alert("Tienes " + introduceTuEdad + " años, puedes entrar a esta página.");
         break
 
     }
