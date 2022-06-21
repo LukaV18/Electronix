cfeae80385e0b4b453299dd3c378a9255c4d79c8
Luka Vitale <lukavitale9@gmail.com>
2022-06-21 20:32:57 -0300

    Primer intento de simulador ecommerce

--- Js/main.js
+++ Js/main.js
@@ -1,19 +1,58 @@
-let introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
+let producto = "";
+let notebook = 20000;
+let tablet = 7000;
+let monitor = 15000;
+let cargadores = 600;
+let precio = 0;
+let cantidad = 0;
+let envio = 150;
+let precioTotal = 0;
+let VolverComprar = false;
 
-while (isNaN(introduceTuEdad) <= 17) {
+alert("¡Bienvenido a Electronix, los mejores productos electrónicos de Bs As!");
 
-    if (isNaN(introduceTuEdad)) {
-        alert("Por favor introduzca solo números.");
-        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
+let NombreIngresado = prompt("Para comenzar nos gustaría saber su nombre:")
 
-    } else if ((introduceTuEdad) < 18) {
+console.log(NombreIngresado);
 
-        alert("Tienes " + introduceTuEdad + " años,no tienes permitido ingresar a esta página.")
-        introduceTuEdad = prompt("Por favor Ingrese su edad, debe ser mayor a 18 años para poder ingresar.");
-    } else {
-        alert("Tienes " + introduceTuEdad + " años, puedes entrar a esta página.");
-        break
+alert("Bienvenido a Electronix " + NombreIngresado + ".")
 
+do {
+    producto = prompt(`Bienvenidos a MrBurger \n ¿Que desea pedir?
+    Notebook / precio=${notebook}
+    Tablet / precio=${tablet}
+    Monitor / precio=${monitor}
+    Cargadores / precio=${cargadores}`)
+    cantidad = Number(prompt("¿Cuantos desea llevar?"));
+    switch (producto) {
+        case "Notebook":
+            precio = notebook //precio = 20000
+            break;
+        case "Tablet":
+            precio = tablet //precio = 7000
+            break;
+        case "Monitor":
+            precio = monitor //precio = 15000
+            break;
+        case "Cargadores":
+            precio = cargadores //precio = 600
+            break;
+        default:
+            alert("No has ingresado un producto valido, de en aceptar y vuelva a intentar.")
+            break;
     }
 
-}
+    function multiplicar(precio, cantidad) {
+        precioTotal += precio * cantidad;
+    }
+
+    function MostrarPrecioTotal(precioTotal) {
+        console.log(precioTotal);
+    }
+    multiplicar(precio, cantidad);
+    MostrarPrecioTotal(precioTotal);
+
+    VolverComprar = confirm(`Su total de compra es de $${precioTotal}\n¿Desea llevar algo más?\nDe en aceptar para continuar su compra o en cancelar para finalizar la compra.`)
+} while (VolverComprar);
+
+alert("Total de su compra en espera es de: $" + precioTotal);
