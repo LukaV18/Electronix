50949efca82de836e6ec3a48defe58427594b3e5
Luka Vitale <lukavitale9@gmail.com>
2022-07-14 20:49:44 -0300

    orden de codigo general

---
+++ Js/data.js
@@ -0,0 +1,143 @@
+//objetos//
+
+let shopItemsData = [{
+        id: 1,
+        name: "Tablet EXO Wave I716 1",
+        precio: 160,
+        img: "./assets/img/tablet.jpg",
+        rating: `<div class="d-flex flex-row justify-content-center mb-3">
+    <div class="ratings2 me-2"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i><i
+            class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
+</div>`
+    },
+    {
+        id: 2,
+        name: "Tablet EXO Wave I716 2",
+        precio: 175,
+        img: "./assets/img/tablet.jpg",
+        rating: `<div class="d-flex flex-row justify-content-center mb-3">
+    <div class="ratings2 me-2"><i class="fa fa-star"></i><i
+            class="fa fa-star"></i><i class="fa fa-star"></i></i></div>
+</div>`
+    },
+    {
+        id: 3,
+        name: "Tablet EXO Wave I716 3",
+        precio: 150,
+        img: "./assets/img/tablet.jpg",
+        rating: `<div class="d-flex flex-row justify-content-center mb-3">
+    <div class="ratings2 me-2"></i><i class="fa fa-star"></i><i
+            class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
+</div>`
+    },
+    {
+        id: 4,
+        name: "Tablet EXO Wave I716 4",
+        precio: 130,
+        img: "./assets/img/tablet.jpg",
+        rating: `<div class="d-flex flex-row justify-content-center mb-3">
+    <div class="ratings2 me-2"><i class="fa fa-star"></i><i class="fa fa-star"></i><i
+            class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
+</div>`
+    }
+]
+
+//objetos ofertas//
+let shopItemsDataOfertas = [{
+        id: 5,
+        name: "Notebook Lenovo IdeaPad 15ITL6",
+        precio: 130,
+        descuento: 199,
+        descripcion: `<div class="descripcion">
+    <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
+            class="dot"></span><span>Es
+            táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
+    </div>`,
+
+        productoTexto: `<div class="productoTexto">
+    <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
+        tu
+        vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
+        la
+        convertirá en tu PC favorita.<br><br>
+    </p>
+</div>`,
+        img: "./assets/img/Notebook2.jpg",
+        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa-solid fa-star-half-stroke"></i></div>`
+    },
+
+    {
+        id: 6,
+        name: "Quant trident shirts",
+        precio: 240,
+        descuento: 300,
+        img: "./assets/img/Notebook3.jpg",
+        descripcion: `<div class="descripcion">
+    <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
+            class="dot"></span><span>Es
+            táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
+    </div>`,
+
+        productoTexto: `<div class="productoTexto">
+    <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
+        tu
+        vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
+        la
+        convertirá en tu PC favorita.<br><br>
+    </p>
+</div>`,
+
+        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>
+    </div>`
+    },
+
+    {
+        id: 7,
+        name: "Quant tinor shirts",
+        precio: 160,
+        descuento: 230,
+        descripcion: `<div class="descripcion">
+    <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
+            class="dot"></span><span>Es
+            táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
+    </div>`,
+
+        productoTexto: `<div class="productoTexto">
+    <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
+        tu
+        vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
+        la
+        convertirá en tu PC favorita.<br><br>
+    </p>
+</div>`,
+
+        img: "./assets/img/Notebook4.jpg",
+        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa-solid fa-star-half-stroke"></i></div>`
+    },
+
+    {
+        id: 8,
+        name: "Notebook Lenovo IdeaPad 15ITL6",
+        precio: 160,
+        descuento: 190,
+        descripcion: `<div class="descripcion">
+    <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
+            class="dot"></span><span>Es
+            táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
+    </div>`,
+
+        productoTexto: `<div class="productoTexto">
+    <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
+        tu
+        vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
+        la
+        convertirá en tu PC favorita.<br><br>
+    </p>
+</div>`,
+
+        img: "./assets/img/Screenshot_5.jpg",
+        rating: `<div class="d-flex flex-row">
+    <div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></div>
+</div>`
+    }
+]
--- Js/main.js
+++ Js/main.js
@@ -1,149 +1,10 @@
-let shop = document.getElementById('shop')
 let shopOfertas = document.getElementById('shopOfertas')
 
 console.log(shopOfertas)
-console.log(shop)
 
-let shopItemsData = [{
-        id: 1,
-        name: "Tablet EXO Wave I716 1",
-        precio: 160,
-        img: "./assets/img/tablet.jpg",
-        rating: `<div class="d-flex flex-row justify-content-center mb-3">
-        <div class="ratings2 me-2"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i><i
-                class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
-    </div>`
-    },
-    {
-        id: 2,
-        name: "Tablet EXO Wave I716 2",
-        precio: 175,
-        img: "./assets/img/tablet.jpg",
-        rating: `<div class="d-flex flex-row justify-content-center mb-3">
-        <div class="ratings2 me-2"><i class="fa fa-star"></i><i
-                class="fa fa-star"></i><i class="fa fa-star"></i></i></div>
-    </div>`
-    },
-    {
-        id: 3,
-        name: "Tablet EXO Wave I716 3",
-        precio: 150,
-        img: "./assets/img/tablet.jpg",
-        rating: `<div class="d-flex flex-row justify-content-center mb-3">
-        <div class="ratings2 me-2"></i><i class="fa fa-star"></i><i
-                class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
-    </div>`
-    },
-    {
-        id: 4,
-        name: "Tablet EXO Wave I716 4",
-        precio: 130,
-        img: "./assets/img/tablet.jpg",
-        rating: `<div class="d-flex flex-row justify-content-center mb-3">
-        <div class="ratings2 me-2"><i class="fa fa-star"></i><i class="fa fa-star"></i><i
-                class="fa fa-star"></i><i class="fa-solid fa-star-half-stroke"></i></i></div>
-    </div>`
-    }
-]
 
-let shopItemsDataOfertas = [{
-        id: 5,
-        name: "Notebook Lenovo IdeaPad 15ITL6",
-        precio: 130,
-        descuento: 199,
-        descripcion: `<div class="descripcion">
-        <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
-                class="dot"></span><span>Es
-                táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
-        </div>`,
 
-        productoTexto: `<div class="productoTexto">
-        <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
-            tu
-            vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
-            la
-            convertirá en tu PC favorita.<br><br>
-        </p>
-    </div>`,
-        img: "./assets/img/Notebook2.jpg",
-        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa-solid fa-star-half-stroke"></i></div>`
-    },
-
-    {
-        id: 6,
-        name: "Quant trident shirts",
-        precio: 240,
-        descuento: 300,
-        img: "./assets/img/Notebook3.jpg",
-        descripcion: `<div class="descripcion">
-        <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
-                class="dot"></span><span>Es
-                táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
-        </div>`,
-
-        productoTexto: `<div class="productoTexto">
-        <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
-            tu
-            vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
-            la
-            convertirá en tu PC favorita.<br><br>
-        </p>
-    </div>`,
-
-        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>
-        </div>`
-    },
-
-    {
-        id: 7,
-        name: "Quant tinor shirts",
-        precio: 160,
-        descuento: 230,
-        descripcion: `<div class="descripcion">
-        <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
-                class="dot"></span><span>Es
-                táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
-        </div>`,
-
-        productoTexto: `<div class="productoTexto">
-        <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
-            tu
-            vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
-            la
-            convertirá en tu PC favorita.<br><br>
-        </p>
-    </div>`,
-
-        img: "./assets/img/Notebook4.jpg",
-        rating: `<div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa-solid fa-star-half-stroke"></i></div>`
-    },
-
-    {
-        id: 8,
-        name: "Notebook Lenovo IdeaPad 15ITL6",
-        precio: 160,
-        descuento: 190,
-        descripcion: `<div class="descripcion">
-        <div class="mt-1 mb-1 spec-1"><span>Es antirreflejo</span><span
-                class="dot"></span><span>Es
-                táctil</span><span class="dot"></span><span>Pad numérico<br></span></div>
-        </div>`,
-
-        productoTexto: `<div class="productoTexto">
-        <p class="text-justify  textoDesc">La notebook Lenovo IdeaPad 3 fue pensada para hacer
-            tu
-            vida más sencilla. Su diseño elegante e innovador y su comodidad para transportarla,
-            la
-            convertirá en tu PC favorita.<br><br>
-        </p>
-    </div>`,
-
-        img: "./assets/img/Screenshot_5.jpg",
-        rating: `<div class="d-flex flex-row">
-        <div class="ratings me-2"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></div><span>110</span>
-    </div>`
-    }
-]
+let CARRITO = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShopOfertas = () => {
     return (shopOfertas.innerHTML = shopItemsDataOfertas.map((productoOfertas) => {
@@ -183,8 +44,37 @@ let generateShopOfertas = () => {
     }).join(""))
 }
 
+generateShopOfertas()
 
-let CARRITO = JSON.parse(localStorage.getItem("data")) || [];
+let shop = document.getElementById('shop')
+
+console.log(shop)
+
+let generateShop = () => {
+    return (shop.innerHTML = shopItemsData.map((producto) => {
+        let {
+            id,
+            name,
+            precio,
+            img,
+            rating
+        } = producto;
+        let search = CARRITO.find((x) => x.id === id) || [];
+        return `
+        <div class="card producto m-4 productoSombra" id="product-id-${id}">
+            <img class="card-img-top" src="${img}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-start fs-4">$
+                ${precio}</h5>
+                <p class="card-text">${name}</p>
+                ${rating}
+                <a class="btn btn-primary" onclick="comprar(${id})">Agregar al carrito</a>
+            </div>
+        </div>`
+    }).join(""))
+}
+
+generateShop()
 
 let comprar = (id) => {
     let selectedItem = id;
@@ -222,29 +112,6 @@ let remover = (id) => {
 }
 
 
-let generateShop = () => {
-    return (shop.innerHTML = shopItemsData.map((producto) => {
-        let {
-            id,
-            name,
-            precio,
-            img,
-            rating
-        } = producto;
-        let search = CARRITO.find((x) => x.id === id) || [];
-        return `
-        <div class="card producto m-4 productoSombra" id="product-id-${id}">
-            <img class="card-img-top" src="${img}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title text-start fs-4">$
-                ${precio}</h5>
-                <p class="card-text">${name}</p>
-                ${rating}
-                <a class="btn btn-primary" onclick="comprar(${id})">Agregar al carrito</a>
-            </div>
-        </div>`
-    }).join(""))
-}
 
 
 let calculador = () => {
@@ -252,8 +119,8 @@ let calculador = () => {
     cartIcon.innerHTML = (CARRITO.map((x) => x.item).reduce((x, y) => x + y, 0))
 
 }
-generateShop()
-generateShopOfertas()
+
+
 
 calculador()
 
